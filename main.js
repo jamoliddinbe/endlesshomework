b1cf488dd0c4b983f1d00026b1d74a680c7d1fb0
jamoliddinbe <bosuvay1@gmail.com>
2022-09-18 22:19:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,371 @@
+// data types 
+
+
+// 1
+
+// let l = prompt('Uzunligini kiriting')
+// let metr = l/100
+// if (l>0) {
+//     console.log(metr);  
+// }
+
+
+
+// 2
+
+// let savol = prompt ('kilogrammi kirit')
+// let tonna = 1000
+
+// if ( savol > 0 ){
+//     console.log(savol / tonna);
+// }
+
+
+// 3
+
+// let a = prompt ("Berilgan kesma uzunligi")
+// let b = prompt ("Mayda kesmalar")
+
+// if (a > b){
+//     console.log(Math.floor (a/b));
+// }
+
+// 4
+
+// let a = prompt ("ikki honali son kiriting")
+
+// if(a>9 && a <100){
+//     console.log(Math.floor (a/10) );
+//     console.log(a%10);
+// }
+
+// 5
+
+// let a = prompt('')
+// if (a > 9 && a < 100 ) {
+//     b = Math.floor(a/10)
+//     c = a % 10
+//     console.log(b+c);
+// }
+
+// 6
+
+// let a = prompt('')
+// if (a > 9 && a < 100 ){
+//     b = Math.floor(a/10)
+//     c = a % 10
+//     console.log(b*c);
+// }
+
+// 7
+// function sum(){
+//     var a = 3, b = 5, sum = 0;
+//     for(var i = a; i <= b; i++){
+//         sum += i;
+//     }
+//     console.log(sum);
+// }
+// sum()
+
+// 8
+
+// function time(a){
+
+//     let time = Math.floor(a/3600)
+//     let min = a%3600
+    
+//         return `${time} soat ${min/60} minut o'tdi`
+//     }
+    
+//     console.log(time(3700))
+
+
+ 
+// SWITCH 
+
+// 1
+
+// let numbers =  1
+// switch (numbers) {
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("wednesday");
+//         break;
+//         case 4:
+//         console.log("thirsday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;  
+//         case 6:
+//         console.log("saturday");
+//         break;  
+//         case 7:
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         console.log("Ooops, anything not found");
+//         break;
+// }
+ 
+
+// 2
+
+// let k = 5
+// switch (k) {
+//     case 1:
+//         console.log("yomon");
+//         break;
+//         case 2:
+//     console.log("qoniqarsiz");
+//     break;
+//     case 3:
+//          console.log("qoniqarli");
+//          break;  
+//          case 4:
+//         console.log("yaxshi");
+//         break; 
+//         case 5:
+//         console.log("alo");
+//         break;     
+//     default:
+//         console.log("sen vaabshe 0san");
+//         break;
+// }
+
+// 3
+
+// let month = prompt("Oyning sonini kiriting")
+// switch (month) {
+//     case 12:
+//         console.log("Winter");
+//         break;
+//         case 1:
+//             console.log("Winter");
+//             break;
+//             case 2:
+//                 console.log("Winter");
+//                 break;
+//                 case 3:
+//                     console.log("Spring");
+//                     break;
+//                     case 4:
+//                         console.log("Spring");
+//                         break;
+//                         case 5:
+//                             console.log("Spring");
+//                             break;
+//                             case 6:
+//                                 console.log("Summer");
+//                                 break;
+//                                 case 7:
+//                                     console.log("Summer");
+//                                     break;
+//                                     case 8:
+//                                         console.log("Summer");
+//                                         break;
+//                                         case 9:
+//                                             console.log("Autumn");
+//                                             break;
+//                                             case 10:
+//                                                 console.log("Autumn");
+//                                                 break;
+//                                                 case 11:
+//                                                     console.log("Autumn");
+//                                                     break;
+//     default:
+//         console.log("ERROR");
+//         break;
+// }
+
+// 4
+
+// let month = 2
+// switch (month) {
+//     case 1:
+//         console.log(31);
+//         break;
+//         case 2:
+//             console.log(28);
+//             break;
+//             case 3:
+//                 console.log(30);
+//                 break;
+//                 case 4:
+//                     console.log(31);
+//                     break;
+//                     case 5:
+//                         console.log(29);
+//                         break;
+//                         case 6:
+//                             console.log(30);
+//                             break;
+//                             case 7:
+//                                 console.log(31);
+//                                 break;
+//                                 case 8:
+//                                     console.log(30);
+//                                     break;
+//                                     case 9:
+//                                         console.log(31);
+//                                         break;
+//                                         case 10:
+//                                             console.log(30);
+//                                             break;
+//                                             case 11:
+//                                                 console.log(31);
+//                                                 break;
+//                                                 case 12:
+//                                                     console.log(30);
+//                                                     break;
+//     default:
+//         console.log("error");
+//         break;
+// }
+
+// 5
+
+
+// let a = prompt("amal kirit")
+// let b = 3, 
+//     c = 4;
+// switch (a) {
+//     case '+':
+//         console.log(c + b);
+//         break;
+//     case '-':
+//         console.log(c - b);
+//         break;
+//     case '%':
+//          console.log(c % b);
+//          break;  
+//      case '*':
+//         console.log(c * b);
+//      break;      
+//     default:
+//         break;
+// }
+
+
+// 6
+
+// let mark = 5
+// switch (mark) {
+//     case 1:
+//         console.log(100000);
+//         break;
+//         case 2:
+//             console.log(200000);
+//             break;
+//             case 3:
+//                 console.log(300000);
+//                 break;
+//                 case 4:
+//                     console.log(400000);
+//                     break;
+//                     case 5:
+//                         console.log(500000);
+//                         break;
+//     default:
+//         console.log(0);
+//         break;
+// }
+
+
+// let a = 10
+// let b = 1
+// let count = 0
+// for (i = b; i<a; i++ ){
+//     console.log(count += i);
+// }
+
+
+// FOR 
+
+
+// 8
+
+
+// function main(a){
+
+//     for(var i = 1;i<=a;i++){
+//       for(let j = 1;j<=i;j++){
+//         var sum = j
+//         sum *=j
+      
+//       }
+  
+//       sum *=sum
+      
+//     }
+//    return sum
+//   }
+//   console.log(main(1))
+
+
+
+
+
+
+// 9
+
+// function main(a){
+
+//     for(var i = 1;i<=a;i++){
+//       for(let j = 1;j<=i;j++){
+//         var sum = j
+//         sum *=j*j
+      
+//       }
+  
+//       sum +=sum
+      
+//     }
+//    return sum
+//   }
+//   console.log(main(5))
+
+
+
+
+
+
+// 11 and 12 
+
+
+
+// const main =  (a) => {
+//     for(let i = 1;i<=a;i++){
+//         if(i%2 === 0){
+//          console.log(i + '---- juft son')
+//         }else{
+//          console.log(i + '---- toq son')
+//         }
+//     }
+// }
+
+// console.log(main(100))
+
+
+
+// 13 and 14
+
+
+// let a = prompt('son kiriting')
+
+
+// if(a%2 == 0 && a/a == 1){
+//     console.log('bu murakkab son')
+// }else{
+//     console.log('bu tub son')
+// }
+
+// 15
+
+
